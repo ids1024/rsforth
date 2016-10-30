dfabae1539ae69931ebef075c7d4846eda5f1811
Ian Douglas Scott <ian@iandouglasscott.com>
2016-10-30 11:28:57 -0700

    Implement very primitive repl

--- src/main.rs
+++ src/main.rs
@@ -14,16 +14,27 @@ use stack::Stack;
 use dictionary::Dictionary;
 
 fn main() {
-    let path = env::args().nth(1).unwrap();
-    let mut file = File::open(&path).unwrap();
-    let mut code = String::new();
-    file.read_to_string(&mut code).unwrap();
-
     let mut dict = Dictionary::default();
-    let branches = parse(&mut code.chars(), &mut dict);
-    // println!("{:#?}", parse(&mut code.chars()));
     let mut stack = Stack::default();
-    for branch in branches {
-        branch.call(&mut stack);
+
+    if let Some(path) = env::args().nth(1) {
+        let mut file = File::open(&path).unwrap();
+        let mut code = String::new();
+        file.read_to_string(&mut code).unwrap();
+
+        let branches = parse(&mut code.chars(), &mut dict);
+        for branch in branches {
+            branch.call(&mut stack);
+        }
+    } else {
+        // Primitive REPL
+        let stdin = std::io::stdin();
+        for line in stdin.lock().lines() {
+            let branches = parse(&mut line.unwrap().chars(), &mut dict);
+            for branch in branches {
+                branch.call(&mut stack);
+            }
+            std::io::stdout().flush().unwrap();
+        }
     }
 }
