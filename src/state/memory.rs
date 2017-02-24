3421a81f8c939765f8201f6d1288d1a70aaffa44
Ian Douglas Scott <ian@iandouglasscott.com>
2017-02-24 10:08:20 -0800

    Add newlines between functions

--- src/state/memory.rs
+++ src/state/memory.rs
@@ -15,16 +15,19 @@ impl Memory {
             panic!("Invalid memory address");
         }
     }
+
     pub fn set<T: Into<ForthNum>>(&mut self, addr: i32, value: T) {
         if addr as usize > self.values.len() - 1 {
             panic!("Invalid memory address");
         }
         self.values[addr as usize] = value.into();
     }
+
     pub fn new<T: Into<ForthNum>>(&mut self, value: T) -> i32 {
         self.values.push(value.into());
         self.here()
     }
+
     pub fn here(&self) -> i32 {
         self.values.len() as i32 - 1
     }
