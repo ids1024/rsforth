a0323273b6dd40891e9b97eaac5c8deb16a3b359
Ian Douglas Scott <ian@iandouglasscott.com>
2021-05-07 19:46:43 -0700

    Use `VecDeque`

--- src/rliterchar.rs
+++ src/rliterchar.rs
@@ -1,12 +1,12 @@
 use rustyline::error::ReadlineError;
-use std::iter::once;
+use std::collections::VecDeque;
 
 /// Provides a wrapper around the rustyline library that behaves as a char
 /// iterator, so it can be passed to the parser function that accept
 /// char iterators.
 pub struct RLIterChar {
     rl: rustyline::Editor<()>,
-    chars: Vec<char>,
+    chars: VecDeque<char>,
     first: bool,
 }
 
@@ -14,7 +14,7 @@ impl RLIterChar {
     pub fn new() -> RLIterChar {
         RLIterChar {
             rl: rustyline::Editor::<()>::new(),
-            chars: Vec::new(),
+            chars: VecDeque::new(),
             first: true,
         }
     }
@@ -24,7 +24,7 @@ impl Iterator for RLIterChar {
     type Item = char;
 
     fn next(&mut self) -> Option<char> {
-        if let Some(c) = self.chars.pop() {
+        if let Some(c) = self.chars.pop_front() {
             Some(c)
         } else {
             if self.first {
@@ -36,8 +36,9 @@ impl Iterator for RLIterChar {
             match self.rl.readline("> ") {
                 Ok(line) => {
                     self.rl.add_history_entry(&line);
-                    self.chars = line.chars().chain(once('\n')).rev().collect();
-                    self.chars.pop()
+                    self.chars.extend(line.chars());
+                    self.chars.push_back('\n');
+                    self.chars.pop_front()
                 }
                 Err(ReadlineError::Interrupted) | Err(ReadlineError::Eof) => None,
                 _ => panic!("Readline error"), // TODO: handle better
